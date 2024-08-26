066755b767d70f3573a3971a207203e179b24e1c
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-08-26 21:19:40 +0800

    compleate db

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import os
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import declarative_base, declared_attr
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 host = os.getenv('DB_HOST')
 port = os.getenv('DB_PORT')
@@ -14,3 +14,15 @@ DB_PORT = port
 DB_NAME = name
 DB_USER = user
 DB_PASSWORD = password
+
+DB_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_async_engine(DB_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
