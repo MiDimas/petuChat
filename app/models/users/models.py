3fe8607e31b3a6d00325ebc9fbd3307a84817f0e
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-08-28 21:10:57 +0800

    added emails model

--- app/models/users/models.py
+++ app/models/users/models.py
@@ -1,9 +1,16 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
 
 class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(300), unique=True)
     password: Mapped[str] = mapped_column(String(300))
-    email: Mapped[str] = mapped_column(String(400))
+    emails: Mapped[list["Email"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+class Email(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped["User"] = relationship(back_populates='emails')
