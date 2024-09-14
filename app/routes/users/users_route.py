8588955db88a5e1a506e02fb82f96a2d4ade2690
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-09-14 18:01:42 +0800

    fixex param request users

--- app/routes/users/users_route.py
+++ app/routes/users/users_route.py
@@ -5,6 +5,6 @@ from app.models.user import User, UsersGetAll
 router = APIRouter(prefix='/users', tags=['Пользователи'])
 
 
-@router.get("/", summary="Получение всех пользователей", response_model=list[UserSchema])
-async def get_all_users(request_body: UsersGetAll):
+@router.get("/{user_id}", summary="Получение всех пользователей", response_model=list[UserSchema])
+async def get_all_users(user_id: int):
     return await User.get_all_users(request_body)
