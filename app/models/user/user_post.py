f93ca5434b7847f4ff6cdaa1136fd589f92b424b
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-10-02 20:04:47 +0300

    added first validation on password and login

--- app/models/user/user_post.py
+++ app/models/user/user_post.py
@@ -1,6 +1,29 @@
-from pydantic import BaseModel, Field
+import re
+
+from pydantic import BaseModel, Field, field_validator
 
 
 class UserCreateData(BaseModel):
-    name: str = Field(min_length=3)
-    password: str = Field(min_length=3)
+    name: str = Field(min_length=3, max_length=30)
+    password: str = Field(min_length=3, max_length=256)
+
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, values: str) -> str:
+        values = values.strip()
+        if len(values) < 3 or len(values) > 30:
+            raise ValueError('Логин не может содержать меньше 3 и больше 30 символов')
+        if not re.match(r'^[A-Za-z0-9._-]$', values):
+            raise ValueError('Логин может содержать только латинские буквы, цифры и символы ".-_"')
+        reserved_names = ['root', 'admin', 'support']
+        if values.lower() in reserved_names:
+            raise ValueError('Логин не может использовать зарезервированное имя')
+        return values
+
+    @field_validator("password")
+    @classmethod
+    def validate(cls, values: str) -> str:
+        if not re.match(r'(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}', values):
+            raise ValueError('''Пароль должен содержать как минимум одну заглавную, 
+            одну прописную буквы латинского алфавита, а так же одну цифру и содержать не менее 8 символов''')
+        return values
