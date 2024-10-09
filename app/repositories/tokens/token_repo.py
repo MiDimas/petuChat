32d944b4fd82acde4ea0d4582d8a3311267da9de
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-10-09 21:02:56 +0300

    added method find token

--- app/repositories/tokens/token_repo.py
+++ app/repositories/tokens/token_repo.py
@@ -4,6 +4,7 @@ from app.database import async_session_maker
 from sqlalchemy import select
 from app.repositories import User
 from datetime import datetime
+from passlib.context import CryptContext
 
 
 class TokenRepo(BaseRepo):
@@ -14,7 +15,7 @@ class TokenRepo(BaseRepo):
                                     info: str | None = None, revoked: bool = False):
         async with async_session_maker() as session:
             query = select(User).where(User.id == user_id)
-            user = session.scalars(query).one()
+            user = await session.scalars(query).one()
             if not user:
                 raise ValueError('Пользователь с таким id не найден')
             token_obj = Token(
@@ -25,5 +26,15 @@ class TokenRepo(BaseRepo):
                 revoked=revoked
             )
             user.tokens.append(token_obj)
-            session.commit()
+            await session.commit()
             return token_obj
+
+    @classmethod
+    async def find_token_obj(cls, token: str):
+        async with async_session_maker() as session:
+            pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
+
+            token_hash = pwd_context.hash(token)
+            query = select(Token).where(Token.refresh_token == token_hash)
+            token_obj = await session.execute(query)
+            return token_obj.scalar_one_or_none()
